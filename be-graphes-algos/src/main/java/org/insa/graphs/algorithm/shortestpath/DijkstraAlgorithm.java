23d295d9882b3d964ccf9885839d5095de1d5983
florianleon <leon-florian@hotmail.fr>
2020-04-04 00:55:48 +0200

    Fin implementation Djikstra
    Normalement ça marche…

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -1,12 +1,17 @@
 package org.insa.graphs.algorithm.shortestpath;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
+import org.insa.graphs.algorithm.AbstractSolution.Status;
 import org.insa.graphs.algorithm.utils.BinaryHeap;
+import org.insa.graphs.algorithm.utils.ElementNotFoundException;
 import org.insa.graphs.model.Arc;
 import org.insa.graphs.model.Graph;
 import org.insa.graphs.model.Node;
+import org.insa.graphs.model.Path;
 
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
@@ -45,6 +50,8 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
         //sommet de depart à 0
         final Node origin = data.getOrigin();
         final Node destination = data.getDestination();
+        final Label originLabel = labels[data.getOrigin().getId()];
+        final Label destinationLabel = labels[data.getDestination().getId()];
         labels[origin.getId()].setCost(0);
         
         //initialisation du tas
@@ -54,42 +61,75 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
         bheap.insert(labels[origin.getId()]);
         
         //to be continued…
+        int compteur = 0;
+        Label x = null;
+        
+        while (!labels[destination.getId()].isMarked() && !bheap.isEmpty()) {
+        	compteur++;
+        	x = bheap.deleteMin();
+        	x.setMarked();
+        	notifyNodeMarked(x.getNode());
+        	
+        	List<Arc> successorsList = x.getNode().getSuccessors();
+        	
+        	for(Arc succ : successorsList) {
+        		
+        		Label y = labels[succ.getDestination().getId()];
+        		notifyNodeReached(succ.getDestination());
+        		
+        		if((!y.isMarked()) && data.isAllowed(succ)) {
+        			
+        			if(y.getCost() > x.getCost() + data.getCost(succ)) {
+        				
+        				//double yCost = x.getCost() + data.getCost(succ);
+        				
+        				double formerCost;
+        		    	double newCost;
+        		    	Arc arcFather;
+        		    	arcFather = succ;
+        		    	formerCost = y.getCost();
+        		    	newCost = x.getCost() + data.getCost(arcFather);
+        		    	
+        		    	if(newCost < formerCost) {
+        		    		/*
+        		    		 * Si l'element existe dans le tas on l'enleve
+        		    		 * s'il existe pas on catch l'erreur et on ne fait rien et du coup on insere dans le tas
+        		    		 */
+        		    		try {
+        		    			bheap.remove(y);
+        		    		} catch(ElementNotFoundException e) {
+        		    			
+        		    		}
+        		    		y.setCost(x.getCost() + arcFather.getLength());
+        		    		y.setArcFather(arcFather);;
+        		    		bheap.insert(y);
+        		    	}
+        				
+        			}	
+        		}
+        	}
+        }
+        
+        if(destinationLabel.isMarked() == false) {
+        	solution = new ShortestPathSolution(data, Status.INFEASIBLE);
+        } else {
+        	ArrayList<Arc> arcs = new ArrayList<Arc>();
+        	while(!x.equals(originLabel)) {
+        		arcs.add(x.getArcFather());
+        		x = labels[x.getArcFather().getOrigin().getId()];
+        	}
+            Collections.reverse(arcs);
+            solution = new ShortestPathSolution(data, Status.OPTIMAL, new Path(graph, arcs));
+            
+        }
         
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        boolean found = false;
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+     
         
         return solution;
     }
 
+    
+    
+    
 }
